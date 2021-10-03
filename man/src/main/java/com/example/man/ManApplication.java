326a15830c33050e1698f6ddcfbf5ece98b1d7be
Stanley Lante Lamptey <36686061+qgh114@users.noreply.github.com>
2021-10-04 00:52:39 +0200

    Add files via upload

---
+++ man/src/main/java/com/example/man/ManApplication.java
@@ -0,0 +1,13 @@
+package com.example.man;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ManApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ManApplication.class, args);
+    }
+
+}
---
+++ man/src/test/java/com/example/man/ManApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.man;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ManApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
